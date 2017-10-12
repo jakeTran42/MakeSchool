357ac07f3d489ae02744d934d09787965d1d5183
Jake Tran <jake.tranehs@gmail.com>
2017-10-12 01:40:52 -0700

    first commit

---
+++ CS1/Roulette/roullette.py
@@ -0,0 +1,40 @@
+import random
+
+bank_account = 1000
+
+green = [0, 37]
+red = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+black = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+
+def which_bet():
+    #User pick between color bet vs number or both
+    bet_choice = ''
+    while bet_choice != 'number' or bet_choice != 'color' or bet_choice != 'both':
+        print
+
+
+
+
+def take_color(color):
+
+def take_number(num):
+    
+def take_bet(amount, bank):
+    
+
+    
+
+def roll_ball():
+    random_num = random.randint(0,37)
+    return random_num
+
+take_color(bet_color)
+take_bet(bet_amount, bank_account)
+take_number(bet_number)
+roll_ball()
+
+print(bet_color)
+print(bet_number)
+print(bet_amount)
+print(bank_account)
+print(roll_ball())
---
+++ README.md
@@ -0,0 +1,1 @@
+These are MakeSchool class projects.
