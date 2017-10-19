15385c532cdc90cbd66c79f89d1d7692c450b2d4
jakeTran42 <jake.tranehs@gmail.com>
2017-10-18 23:08:19 -0700

    Web1

---
+++ Web1/custom-project/app.js
@@ -0,0 +1,26 @@
+//-----------------------------VARIABLES----------------------//
+var express = require('express');
+var app = express();
+var exphbs  = require('express-handlebars');
+var games = [
+  {title: "New Games"},
+  {title: "Next Game"}
+]
+
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+
+//-----------------------------ROUTES-------------------------//
+
+app.get('/games', function (req, res) {
+  res.render('games-index', {games: games});
+})
+
+
+//-----------------------------LISTENERS----------------------//
+app.listen(3000, function () {
+  console.log('Gif Search listening on port localhost:3000! Custom Project');
+});
