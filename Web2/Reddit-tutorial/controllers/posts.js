37471e594ff7efaca8bc0676ddf8d6028a973dbe
jakeTran42 <jake.tranehs@gmail.com>
2017-11-12 23:50:09 -0800

    add dependencies

---
+++ Web2/Reddit-tutorial/controllers/posts.js
@@ -0,0 +1,17 @@
+var Post = require('../models/post');
+
+module.exports = function(app) {
+
+  // CREATE
+  app.post('/posts', function (req, res) {
+    // INSTANTIATE INSTANCE OF POST MODEL
+    var post = new Post(req.body);
+
+    // SAVE INSTANCE OF POST MODEL TO DB
+    post.save(function (err, post) {
+      // REDIRECT TO THE ROOT
+      return res.redirect(`/`);
+    })
+  });
+
+};
--- Web2/Reddit-tutorial/index.js
+++ Web2/Reddit-tutorial/index.js
@@ -1,12 +1,21 @@
 var express = require('express')
 var app = express()
 var exphbs  = require('express-handlebars');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
 
 // Middlewares
+mongoose.connect('mongodb://localhost/reddit-clone', { useMongoClient: true });
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+//Requires
+require('./controllers/posts.js')(app);
 
 
 
@@ -14,10 +23,12 @@ app.set('view engine', 'handlebars');
 
 //Routes
 app.get('/', function (req, res) {
-  res.render('home', {msg: 'Hello World!'});
+  res.render('home', {msg: 'Hello Reddit!'});
 })
 
-
+app.get('/posts/new', function (req, res) {
+    res.render('posts-new', {})
+})
 
 
 
