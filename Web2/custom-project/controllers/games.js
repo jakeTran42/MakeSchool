e416f431748f81ca6f01549b8d242a1d02003e2a
Jake Tran <jake.tranehs@gmail.com>
2017-12-12 01:47:41 -0800

    delete not compl

--- Web2/custom-project/controllers/games.js
+++ Web2/custom-project/controllers/games.js
@@ -37,13 +37,25 @@ module.exports = (app) => {
   app.get('/games/:id', function (req, res) {
 
       var currentUser = req.user;
+      var currentAuthor = Game.author;
 
      // LOOK UP THE POST
      Game.findById(req.params.id).populate('author').populate('comments').then((game) => {
-         let currentClass = currentUser === game.author ? "is-author" : "";
-       res.render('game-show', { game, currentUser, currentClass })
-     }).catch((err) => {
-       console.log(err.message)
-     })
+
+          // const currentClass = x = (currentUser, currentAuthor) => {
+          //   if (currentUser === currentAuthor) {
+          //     return 'is-author';
+          //   } else {
+          //     return 'nil';
+          //   }
+          // };
+
+         const currentClass = (currentUser === currentAuthor ? "is-author" : "nil");
+         console.log('current class is: ' + currentClass)
+         
+         res.render('game-show', { game, currentUser, currentClass })
+       }).catch((err) => {
+         console.log(err.message)
+       })
    })
 };
