151a2f302cc0f144015f12cf85be458e22561d25
jakeTran42 <jake.tranehs@gmail.com>
2019-05-15 02:52:00 -0700

    electron added

--- Web-6-Redux/timer-app/src/App.js
+++ Web-6-Redux/timer-app/src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Welcome to Time React</h1>
           </header>
           <AddTimer />
           <Timers />
